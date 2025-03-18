3aefd4c55cf712dbfe766e0770643ecc34093e7e
Micheil <fouriemicheil1@gmail.com>
2025-03-18 12:09:32 +0200

    gh-pages basename

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import {
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Furniture-Shop-Website">
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
